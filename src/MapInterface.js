a3015ae3ba1cd8cc8fba9054cdd310cd390e7bb2
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-08-24 23:37:56 +0530

    :sparkles: Toggle menu created

--- src/MapInterface.js
+++ src/MapInterface.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import Mumbai from "./assets/gateway-of-india-mumbai.svg";
 import Delhi from "./assets/india-gate-delhi.svg";
@@ -17,6 +17,9 @@ function MapInterface() {
   ];
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const [toggleFilter, setToggleFilter] = useState(false);
+  const [builtup, setBuiltup] = useState(false);
+  const [residential, setResidential] = useState(false);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -32,35 +35,93 @@ function MapInterface() {
     <>
       <main>
         <div className="flex">
-          <div ref={mapContainer} className="h-screen w-4/6" />
-          <div className="w-2/6">
-            <div className="h-screen grid place-items-center bg-zinc-100 text-center">
-              <div className="bg-white rounded-xl gap-2 p-2 m-2">
-                <h1 className="font-bold text-2xl p-2">
-                  Select a city to visit
-                </h1>
-                <div className="flex">
-                  {cities.map((city, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="rounded-xl p-2 hover:bg-gray-100"
-                        onClick={() => {
-                          map.current.flyTo({
-                            center: city.coords,
-                            essential: true,
-                            zoom: 10,
-                            duration: 10000,
-                          });
-                        }}
-                      >
-                        <img src={city.icon} alt={city.name}></img>
-                        <p>{city.name}</p>
-                      </div>
-                    );
-                  })}
+          <div ref={mapContainer} className="h-screen w-5/6" />
+          <div className="w-1/6">
+            <div className="h-screen grid place-items-center bg-zinc-100">
+              <div className="p-2 bg-white rounded-xl">
+                <div className="flex gap-4">
+                  <div
+                    className={
+                      toggleFilter
+                        ? "hover:bg-black hover:text-white rounded-xl p-2"
+                        : "bg-black text-white rounded-xl p-2"
+                    }
+                    onClick={() => {
+                      setToggleFilter(false);
+                    }}
+                  >
+                    Cities
+                  </div>
+                  <div
+                    className={
+                      !toggleFilter
+                        ? "hover:bg-black hover:text-white rounded-xl p-2"
+                        : "bg-black text-white rounded-xl p-2"
+                    }
+                    onClick={() => {
+                      setToggleFilter(true);
+                    }}
+                  >
+                    Filters
+                  </div>
                 </div>
               </div>
+              {!toggleFilter ? (
+                <div className="bg-white rounded-xl gap-2 p-2 m-2">
+                  <h1 className="font-bold text-2xl p-2 grid place-items-center text-center">
+                    Select a city <br />
+                    to visit
+                  </h1>
+                  <div className="flex flex-col">
+                    {cities.map((city, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="rounded-xl p-2 hover:bg-gray-100 flex flex-col items-center"
+                          onClick={() => {
+                            map.current.flyTo({
+                              center: city.coords,
+                              essential: true,
+                              zoom: 10,
+                              duration: 10000,
+                            });
+                          }}
+                        >
+                          <img src={city.icon} alt={city.name}></img>
+                          <p>{city.name}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              ) : (
+                <div className="bg-white rounded-xl gap-2 p-2 m-2">
+                  <div class="form-control">
+                    <label class="label cursor-pointer">
+                      <span class="label-text pr-4">Residential</span>
+                      <input
+                        type="checkbox"
+                        class="toggle toggle-primary"
+                        checked={residential}
+                        onClick={() => {
+                          setResidential(!residential);
+                        }}
+                      />
+                    </label>
+                    <label class="label cursor-pointer">
+                      <span class="label-text pr-4">Builtup</span>
+                      <input
+                        type="checkbox"
+                        class="toggle toggle-primary"
+                        checked={builtup}
+                        onClick={() => {
+                          setBuiltup(!builtup);
+                        }}
+                      />
+                    </label>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
