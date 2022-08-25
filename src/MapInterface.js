bf4e4c19f029c3955552d18488256a044094ff13
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-08-25 16:44:18 +0530

    :bug: Remove coord state

--- src/MapInterface.js
+++ src/MapInterface.js
@@ -11,7 +11,7 @@ mapboxgl.accessToken =
 function MapInterface() {
   let cities = [
     { name: "Bengaluru", coords: [77.5946, 12.9716], icon: Bengaluru },
-    { name: "Delhi", coords: [77.216721, 28.6448], icon: Delhi },
+    { name: "Delhi", coords: [77.209, 28.6139], icon: Delhi },
     { name: "Kolkata", coords: [88.3639, 22.5726], icon: Kolkata },
     { name: "Mumbai", coords: [72.8777, 19.076], icon: Mumbai },
   ];
@@ -32,14 +32,13 @@ function MapInterface() {
   };
 
   const [currentCity, setCurrentCity] = useState("Delhi");
-  const [coord, setCoord] = useState(coord_mapping["Delhi"]);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/satellite-streets-v11",
-      center: [77.1025, 28.7041], // Default - Delhi
+      center: cities[1]["coords"], // Default - Delhi
       // maxZoom: 11,
       // minZoom: 4,
       zoom: 12,
@@ -56,13 +55,19 @@ function MapInterface() {
     });
   });
 
-  const addLayer = () => {
+  const addLayer = (coord) => {
     if (typeof map.current.getSource("radar") !== "undefined") {
       map.current.removeLayer("radar-layer");
       map.current.removeSource("radar");
     }
 
     console.log(coord);
+    console.log([
+      [coord[0], coord[3]],
+      [coord[2], coord[3]],
+      [coord[2], coord[1]],
+      [coord[0], coord[1]],
+    ]);
 
     map.current.addSource("radar", {
       type: "image",
@@ -98,7 +103,7 @@ function MapInterface() {
                       Select a city <br />
                       to visit
                     </h1>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                       {cities.map((city, index) => {
                         return (
                           <div
@@ -112,11 +117,10 @@ function MapInterface() {
                               map.current.flyTo({
                                 center: city.coords,
                                 essential: true,
-                                zoom: 10,
+                                zoom: 12,
                                 duration: 10000,
                               });
                               setCurrentCity(city.name);
-                              // setCoord(coord_mapping[city.name]);
                             }}
                           >
                             <img src={city.icon} alt={city.name}></img>
@@ -130,20 +134,20 @@ function MapInterface() {
                           const sw = map.current.getBounds()["_sw"];
 
                           console.log(
+                            "South West",
                             sw["lng"],
                             sw["lat"],
+                            "North East",
                             ne["lng"],
                             ne["lat"]
                           );
 
-                          setCoord([
+                          addLayer([
                             sw["lng"],
                             sw["lat"],
                             ne["lng"],
                             ne["lat"],
                           ]);
-
-                          addLayer();
                         }}
                         className="p-2 bg-violet-400 hover:bg-violet-600 rounded-xl font-semibold text-white"
                       >
